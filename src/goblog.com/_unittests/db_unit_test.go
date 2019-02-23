59e0cfcc8dd37cdeeefe019723be86b9f570137d
chris-malloy <chrismalloymusic@gmail.com>
2019-02-23 10:30:25 -0500

    Connection string test passing.

--- src/goblog.com/_unittests/db_unit_test.go
+++ src/goblog.com/_unittests/db_unit_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"goblog.com/api/db"
+	"os"
 )
 
 var _ = Describe("The Creds Function", func() {
@@ -11,6 +12,30 @@ var _ = Describe("The Creds Function", func() {
 		It("Should fail with an error.", func() {
 			_, err := db.GetCredsFromEnv()
 			Expect(err).ToNot(BeNil())
+		})
+	})
+
+	Context("When passed all necessary creds", func() {
+		BeforeEach(func() {
+			os.Setenv("DB_USER", "samus")
+			os.Setenv("DB_PASS", "aran")
+			os.Setenv("DB_HOST", "localhost")
+			os.Setenv("DB_NAME", "metroid")
+		})
+
+		It("Should convert to a valid connection string", func() {
+			creds, err := db.GetCredsFromEnv()
+			Expect(err).To(BeNil(), "creds error should be nil")
+
+			connStr := creds.ToConnectionString()
+			Expect(connStr).To(Equal("postgres://samus:aran@localhost:5432/metroid"))
+		})
+
+		AfterEach(func() {
+			os.Unsetenv("DB_HOST")
+			os.Unsetenv("DB_USER")
+			os.Unsetenv("DB_PASS")
+			os.Unsetenv("DB_NAME")
 		})
 	})
 })
--- src/goblog.com/api/db/postgres.go
+++ src/goblog.com/api/db/postgres.go
@@ -1,10 +1,49 @@
 package db
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 type Creds struct {
+	host    string
+	user    string
+	pass    string
+	dbname  string
+	options string
 }
 
 func GetCredsFromEnv() (*Creds, error) {
-	return &Creds{}, errors.New("no creds found")
+	host, ok := os.LookupEnv("DB_HOST")
+	if !ok {
+		return nil, errors.New("no host specified in environment")
+	}
+
+	user, ok := os.LookupEnv("DB_USER")
+	if !ok {
+		return nil, errors.New("no user specified in environment")
+	}
+
+	pass, ok := os.LookupEnv("DB_PASS")
+	if !ok {
+		return nil, errors.New("no password specified in environment")
+	}
+
+	dbname, ok := os.LookupEnv("DB_NAME")
+	if !ok {
+		return nil, errors.New("no database specified in environment")
+	}
+
+	options := os.Getenv("DB_OPTN")
+
+	return &Creds{host, user, pass, dbname, options}, nil
+}
+
+const dbConnectionTemplate = `postgres://%s:%s@%s:5432/%s`
+
+func (c Creds) ToConnectionString() string {
+	connStr := fmt.Sprintf(dbConnectionTemplate, c.user, c.pass, c.host, c.dbname)
+
+	return connStr
 }
