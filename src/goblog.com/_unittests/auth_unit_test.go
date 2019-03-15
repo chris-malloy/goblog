71825f6be0c49c574f5dae3b296bd771513a24d4
chris-malloy <chrismalloymusic@gmail.com>
2019-03-14 21:38:45 -0400

    Finished initial unit test for JWT parser.

--- src/goblog.com/_unittests/auth_unit_test.go
+++ src/goblog.com/_unittests/auth_unit_test.go
@@ -2,12 +2,14 @@ package _unittests
 
 import (
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"goblog.com/api/handlers"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"time"
 )
 
 var _ = Describe("JWT Authentication middleware functions", func() {
@@ -18,17 +20,64 @@ var _ = Describe("JWT Authentication middleware functions", func() {
 		})
 
 		It("Doesn't allow access when called with an invalid authorization header.", func() {
+			called := false
 			handler := handlers.RequireLogin()
-			server, called := mockCall(handler)
+			server := handler(func(writer http.ResponseWriter, request *http.Request) {
+				called = true
+				writer.WriteHeader(http.StatusOK)
+			})
 
 			request, _ := http.NewRequest("GET", "/thisdoesnotmatter", nil)
+			request.Header.Set("Authorization", "thisisinvalid")
+
 			recorder := httptest.NewRecorder()
 			server.ServeHTTP(recorder, request)
 
-			request.Header.Set("Authorization", "thisisinvalid")
+			Expect(called).To(BeFalse())
+			Expect(recorder.Code).To(Equal(http.StatusUnauthorized))
+		})
+
+		It("Doesn't allow access when called without a JWT.", func() {
+			called := false
+			handler := handlers.RequireLogin()
+			server := handler(func(writer http.ResponseWriter, request *http.Request) {
+				called = true
+				writer.WriteHeader(http.StatusOK)
+			})
+
+			request, _ := http.NewRequest("GET", "/thisdoesnotmattter", nil)
+			recorder := httptest.NewRecorder()
+			server.ServeHTTP(recorder, request)
 
 			Expect(called).To(BeFalse())
 			Expect(recorder.Code).To(Equal(http.StatusUnauthorized))
+		})
+
+		It("Allows access when called with a valid JWT.", func() {
+			called := false
+			handler := handlers.RequireLogin()
+			server := handler(func(writer http.ResponseWriter, request *http.Request) {
+				called = true
+				writer.WriteHeader(http.StatusOK)
+			})
+
+			secret, _ := os.LookupEnv("JWT_SECRET")
+			mockClaims := jwt.MapClaims{
+				"sub": 1,
+				"iat": time.Now().Unix(),
+				"exp": time.Now().Add(time.Hour * 4).Unix(),
+			}
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, mockClaims)
+			tokenString, _ := token.SignedString([]byte(secret))
+
+			request, _ := http.NewRequest("GET", "/thisdoesnotmatter", nil)
+			request.Header.Set("Authorization", "Bearer "+tokenString)
+
+			recorder := httptest.NewRecorder()
+			server.ServeHTTP(recorder, request)
+
+			Expect(called).To(BeTrue())
+			Expect(recorder.Code).To(Equal(http.StatusOK))
 		})
 	})
 })
--- src/goblog.com/api/handlers/auth_handlers.go
+++ src/goblog.com/api/handlers/auth_handlers.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/husobee/vestigo"
 	"goblog.com/api/utils"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -18,12 +21,52 @@ var requiresLoginMiddleware = func(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func ValidateJWT(writer http.ResponseWriter, request *http.Request, handlerFunc http.HandlerFunc) {
+func ValidateJWT(writer http.ResponseWriter, request *http.Request, next http.HandlerFunc) {
 	tokenParts := strings.Split(request.Header.Get("Authorization"), "Bearer")
 	if len(tokenParts) != 2 {
 		utils.RenderErrorWithStatus(writer, errors.New("validateJWT error: got invalid authorization header"), http.StatusUnauthorized)
 		return
 	}
 
-	return
+	token, err := jwt.Parse(strings.TrimSpace(tokenParts[1]), getAndValidJWTSecret)
+
+	if err != nil {
+		utils.RenderErrorWithStatus(writer, err, http.StatusUnauthorized)
+		return
+	}
+
+	if isTokenValidAfterClaims(token) {
+		next(writer, request)
+	} else {
+		utils.RenderErrorWithStatus(writer, errors.New("invalid JWT token"), http.StatusUnauthorized)
+	}
+}
+
+func getAndValidJWTSecret(token *jwt.Token) (interface{}, error) {
+	// Don't forget to validate the alg is what you expect:
+	if !isSigningMethodOkay(token) {
+		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+	}
+
+	secret, ok := getJWTSecret()
+	if !ok {
+		return nil, fmt.Errorf("JWT secret not found")
+	}
+
+	return secret, nil
+}
+
+func getJWTSecret() ([]byte, bool) {
+	secret, ok := os.LookupEnv("JWT_SECRET")
+	return []byte(secret), ok
+}
+
+func isSigningMethodOkay(token *jwt.Token) bool {
+	_, ok := token.Method.(*jwt.SigningMethodHMAC)
+	return ok
+}
+
+func isTokenValidAfterClaims(token *jwt.Token) bool {
+	_, ok := token.Claims.(jwt.MapClaims)
+	return ok && token.Valid
 }
