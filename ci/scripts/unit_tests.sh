497c701d6e626a2c31e41f29479f6ae9dc7ccb75
chris-malloy <chrismalloymusic@gmail.com>
2019-04-11 20:04:30 -0400

    Running test with go cli.

--- ci/scripts/unit_tests.sh
+++ ci/scripts/unit_tests.sh
@@ -2,7 +2,7 @@
 export GOPATH=$(pwd)
 export GOBIN=$GOPATH/bin
 
-ginkgo watch -r -notify src/goblog.com/_unittests
+#ginkgo watch -r -notify src/goblog.com/_unittests
 
 # run this command if you prefer to not run the suite with ginkgo
-# go test -v goblog.com/_unittests
+ go test -v goblog.com/_unittests
